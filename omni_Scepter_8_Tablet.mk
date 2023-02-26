92f33ef99a586147bcac23cfeb5ec0ed96e36e26
SamuEDL <59443298+SamuEDL@users.noreply.github.com>
2023-02-26 10:18:12 +0100

    Update omni_Scepter_8_Tablet.mk

--- omni_Scepter_8_Tablet.mk
+++ omni_Scepter_8_Tablet.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Scepter_8_Tablet device
 $(call inherit-product, device/qlink/Scepter_8_Tablet/device.mk)
